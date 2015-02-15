93fc21360d94ec275ac7b85ab65b7402e741ef78
Noranda Brown <noranda@norandabrown.com>
2015-02-15 00:16:45 -0500

    added config files

--- Brocfile.js
+++ Brocfile.js
@@ -1,6 +1,9 @@
 /* global require, module */
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var pickFiles = require('broccoli-static-compiler');
+var mergeTrees = require('broccoli-merge-trees');
+var gzipFiles = require('broccoli-gzip');
 
 var app = new EmberApp();
 
@@ -17,4 +20,49 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
-module.exports = app.toTree();
+app.import("bower_components/jquery-cookie/jquery.cookie.js");
+
+// bootstrap
+app.import("bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js");
+
+// // high charts
+// app.import("bower_components/highcharts-release/highcharts.src.js");
+// app.import("bower_components/highcharts-release/highcharts-more.src.js");
+// app.import("bower_components/highcharts-release/modules/solid-gauge.src.js");
+
+// // numeral.js, a number formatting library
+// app.import("bower_components/numeral/numeral.js");
+// app.import("bower_components/numeral/languages.js");
+
+// moment.js, a time formatting library
+app.import("bower_components/moment/moment.js");
+
+var bootstrapFonts = pickFiles('bower_components/bootstrap-sass-official/assets/fonts/bootstrap', {
+  srcDir: '/',
+  destDir: '/assets/bootstrap'
+});
+
+var fontawesomeFonts = pickFiles('bower_components/fontawesome/fonts', {
+  srcDir: '/',
+  destDir: '/assets/fontawesome'
+});
+
+// var fontcustomFonts = pickFiles('vendor/fontcustom/fonts', {
+//   srcDir: '/',
+//   destDir: '/assets/fontcustom'
+// });
+
+var allTrees = mergeTrees([app.toTree(), bootstrapFonts, fontawesomeFonts, fontcustomFonts]);
+var finalTree = allTrees;
+
+if (app.env === 'production') {
+  var gzipTree = gzipFiles(allTrees, {
+    extensions: ['html', 'js', 'css', 'xml', 'txt', 'json', 'svg'],
+    keepUncompressed: true
+  });
+
+  finalTree = gzipTree;
+}
+
+module.exports = finalTree;
+
