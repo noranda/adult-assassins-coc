a0fd8c61aff8b84bc2bd4bcb89a0e247c80793a3
Noranda Brown <noranda@norandabrown.com>
2015-02-18 18:45:46 -0500

    updated styles

--- app/models/war.js
+++ app/models/war.js
@@ -13,6 +13,23 @@ export default DS.Model.extend({
     return now.isAfter(startDate) && !this.get('isFinished');
   }.property('startDate', 'isFinished'),
 
+  isPreparing: function() {
+    var startDate = moment(this.get('startDate'));
+    var warStartDate = moment(startDate).add(1, 'days');
+    var now = moment();
+
+    return (now.isAfter(startDate) || now.isSame(startDate)) && now.isBefore(warStartDate);
+  }.property('startDate'),
+
+  isWarring: function() {
+    var startDate = moment(this.get('startDate'));
+    var warStartDate = moment(startDate).add(1, 'days');
+    var endDate = moment(startDate).add(2, 'days');
+    var now = moment();
+
+    return (now.isAfter(warStartDate) || now.isSame(warStartDate)) && now.isBefore(endDate);
+  }.property('startDate', 'isFinished'),
+
   isFinished: function() {
     var endDate = moment(this.get('startDate')).add(2, 'days');
     var now = moment();
