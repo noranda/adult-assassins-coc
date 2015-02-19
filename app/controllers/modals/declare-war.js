5fbdf75896157d784066937532188a0ac4511d3c
Noranda Brown <noranda@norandabrown.com>
2015-02-18 23:18:27 -0500

    updated declare war modal styles

--- app/controllers/modals/declare-war.js
+++ app/controllers/modals/declare-war.js
@@ -3,14 +3,21 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   proxiedSelectedWarSize: null,
   selectedWarSize: Ember.computed.oneWay('proxiedSelectedWarSize.value'),
+  warStatus: null,
+
+  preparationDay: function() {
+    return this.get('warStatus') === 'preparation-day';
+  }.property('warStatus'),
+
+  warDay: function() {
+    return this.get('warStatus') === 'war-day';
+  }.property('warStatus'),
 
   warSizes: function() {
     var sizes = [];
-
     for (var i = 10; i <= 50; i += 5) {
       sizes.push(`${i} vs. ${i}`);
     }
-
     return sizes;
   }.property(),
 
@@ -26,6 +33,10 @@ export default Ember.Controller.extend({
     close: function() {
       this._reset();
       this.send('closeModal');
+    },
+
+    submit: function() {
+      // save war
     }
   }
 });
