37789c76a81bef98e83f43eac38123baef6b139d
Peter Grippi <pgrippi@dresourcesgroup.com>
2015-02-21 09:04:35 -0500

    wip

--- app/controllers/modals/declare-war.js
+++ app/controllers/modals/declare-war.js
@@ -1,9 +1,15 @@
 import Ember from 'ember';
 
+var { isEmpty } = Ember;
+
 export default Ember.Controller.extend({
+  opposingClanName: null,
+  opposingClanTag: null,
+  warStatus: null,
   proxiedSelectedWarSize: null,
   selectedWarSize: Ember.computed.oneWay('proxiedSelectedWarSize.value'),
-  warStatus: null,
+  warHours: null,
+  warMinutes: null,
 
   preparationDay: function() {
     return this.get('warStatus') === 'preparation-day';
@@ -12,6 +18,8 @@ export default Ember.Controller.extend({
   warDay: function() {
     return this.get('warStatus') === 'war-day';
   }.property('warStatus'),
+
+  errors: [],
 
   warSizes: function() {
     var sizes = [];
@@ -26,7 +34,41 @@ export default Ember.Controller.extend({
   }),
 
   _reset: function() {
-    this.set('proxiedSelectedWarSize', null);
+    this.setProperties({
+      opposingClanName: null,
+      opposingClanTag: null,
+      warStatus: null,
+      proxiedSelectedWarSize: null,
+      warHours: null,
+      warMinutes: null,
+      errors: []
+    });
+  },
+
+  _validate: function() {
+    var errors = [];
+
+    if (isEmpty(this.get('opposingClanName'))) {
+      errors.push('Opposing clan name cannot be empty.');
+    }
+    if (isEmpty(this.get('opposingClanTag'))) {
+      errors.push('Opposing clan tag cannot be empty.');
+    }
+    if (isEmpty(this.get('warStatus'))) {
+      errors.push('War status must be selected.');
+    }
+    if (isEmpty(this.get('selectedWarSize'))) {
+      errors.push('Clan war size must be selected.');
+    }
+    if (isEmpty(this.get('warHours'))) {
+      errors.push('Remaining hours of war cannot be empty.');
+    }
+    if (isEmpty(this.get('warMinutes'))) {
+      errors.push('Remaining minutes of war cannot be empty.');
+    }
+
+    this.set('errors', errors);
+    return errors.length === 0;
   },
 
   actions: {
@@ -36,7 +78,10 @@ export default Ember.Controller.extend({
     },
 
     submit: function() {
-      // save war
+      console.debug('submit was called');
+      if (this._validate()) {
+        // save
+      }
     }
   }
 });
